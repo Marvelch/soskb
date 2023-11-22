94ddf2fa63e8348355674beae48ec11bebd10cd1
IT <mr.marvel.christevan@gmail.com>
2023-11-22 13:44:10 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,6 @@
 @include('tamplate.head')
 
 <body class="fixed-bottom-padding">
-    <div class="theme-switch-wrapper">
-        <label class="theme-switch" for="checkbox">
-            <input type="checkbox" id="checkbox" />
-            <div class="slider round"></div>
-            <i class="icofont-moon"></i>
-        </label>
-        <em>Enable Dark Mode!</em>
-    </div>
     <!-- sign in -->
     <div class="osahan-signin">
         <div class="border-bottom p-3 d-flex align-items-center">
@@ -18,12 +10,12 @@
                     class="icofont-rounded-left back-page"></i></a>
         </div>
         <div class="p-3">
-            <img src="{{asset('./img/logo-transparent.png')}}" class="w-100 mb-3" alt="" srcset="">
+            <img src="{{asset('./img/logo-transparent.png')}}" class="w-100 mb-3 mt-4" alt="" srcset="">
              <form method="POST" action="{{ route('login') }}">
                 @csrf
-                <div class="form-group mb-3">
+                <div class="form-group mb-3 mt-3">
                     <label for="exampleInputEmail1">Email</label>
-                    <input id="email" placeholder="Enter Email" type="email" class="form-control @error('email') is-invalid @enderror"
+                    <input id="email" type="email" class="form-control mt-1 @error('email') is-invalid @enderror"
                         aria-describedby="emailHelp" name="email" value="{{ old('email') }}" autofocus>
                     @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -33,14 +25,14 @@
                 </div>
                 <div class="form-group mb-3">
                     <label for="exampleInputPassword1">Password</label>
-                    <input id="password" placeholder="Enter Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    <input id="password" type="password" class="form-control mt-1 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                     @error('password')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
                 </div>
-                <button type="submit" class="btn btn-success btn-lg rounded w-100">Login</button>
+                <button type="submit" class="btn btn-success btn-lg rounded w-100 mt-2">Login</button>
             </form>
         </div>
     </div>
