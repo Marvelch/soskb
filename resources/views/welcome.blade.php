f3403d840e18f12f9b890a0bf38f134b4211564a
IT <mr.marvel.christevan@gmail.com>
2024-01-02 15:47:38 +0700

    version 1.2.04 UI Design Setup and Login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <!-- Include your necessary CSS and meta tags -->
   @include('tamplate.head')
-  <body>
-    <!-- Osahan Index -->
-    <div class="osahan-index">
-      <div
-        class="bg-success d-flex align-items-center justify-content-center vh-100"
-      >
-        <a href="{{route('setup_general')}}"
-          ><img class="index-osahan-logo" src="{{asset('./img/logo.png')}}" alt="" />
-        </a>
-      </div>
-    </div>
-    @include('tamplate.footer')
-  </body>
+  <style>
+    .osahan-index {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      background-color: #28a745; /* You can change this to your desired background color */
+    }
+
+    .index-osahan-logo {
+      /* Add your styles for the logo if needed */
+    }
+  </style>
+</head>
+<body>
+  <!-- Osahan Index -->
+  <div class="osahan-index">
+    <a href="{{route('setup_general')}}">
+      <img class="index-osahan-logo" src="{{asset('./img/logo.png')}}" alt="" />
+    </a>
+  </div>
+  @include('tamplate.footer')
+</body>
 </html>
