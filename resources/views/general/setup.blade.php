c02e1175d649a10e4b2b2c27dae76c27a4f4ac81
IT <mr.marvel.christevan@gmail.com>
2024-01-02 17:05:04 +0700

    version 1.3.00 New Fiture Login With Whatsapp

--- resources/views/general/setup.blade.php
+++ resources/views/general/setup.blade.php
@@ -16,13 +16,57 @@
          </a> -->
         <div class="m-2">
             <div class="form-group">
-                <a href="{{route('login')}}" class="btn bg-success btn-block mb-4 btn-lg" style="border-radius: 10px; color: #fff">
-                    Login
+                <a href="{{route('login')}}">
+                    <div class="card mb-3" style="border-radius: 10px;">
+                        <div class="card-body bg-success" style="border-radius: 10px;">
+                            <div class="d-flex bd-highlight text-white">
+                                <div class="me-auto bd-highlight">Login</span></div>
+                                <div class="bd-highlight"><i class="icofont-arrow-right"></i></div>
+                            </div>
+                        </div>
+                    </div>
                 </a>
+            </div>
+            <div class="form-group text-center">
+                OR Login With
+            </div>
+            <div class="form-group mt-3" data-bs-toggle="modal" data-bs-target="#whatsappModal">
+                <div class="card mb-4" style="border-radius: 10px;">
+                    <div class="card-body bg-success" style="border-radius: 10px;">
+                        <div class="d-flex bd-highlight text-white">
+                            <div class="me-auto bd-highlight"><img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"
+                                    alt="" srcset="" style="width: 20px;"> <span
+                                    style="margin-left: 7px;">Whatsapp</span></div>
+                            <div class="bd-highlight"><i class="icofont-arrow-right"></i></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     @include('tamplate.footer')
+    <!-- Modal -->
+    <div class="modal fade" id="whatsappModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body text-start">
+                    <img src="https://www.sinch.com/sites/default/files/styles/large_hq/public/image/2021-07/Illustration_mobile_whatsapp.png.webp"
+                        class="w-100" alt="" srcset="">
+                    <div class="form-group pt-4">
+                        <label for="">Nomor Telpon</label>
+                        <input type="text" class="form-control form-control-sm shadow">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary">Whatsapp Activation</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
